3caa616d284171f7ee741f5bffd5170d5d4f5de4
Rishav-bot895 <rishav08092005@gmail.com>
2025-12-24 15:17:24 +0530

    Setup template and static dirs

--- tribe/home/views.py
+++ tribe/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    if request=='POST':
+        pass
+    else:
+        return render(request,'index.html')
